6d171a3e6a0710d44dad9c1fca7a4de216ee1b64
SmpManeth <smpmaneth@gmail.com>
2025-05-14 17:51:12 +0530

    build(deps): update App.js, package-lock.json and package.json

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { Text, TouchableOpacity } from "react-native";
+import { Text , StyleSheet } from "react-native";
 import { Profile } from "./components/Profile/Profile";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import {styles} from "./App.styles"
 
 export default function App() {
   return (
-    <>
-      <Text>Welcome to React Native!</Text>
-      <Profile />
-    </>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Text style={ styles.text} >Welcome to React Native!</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
