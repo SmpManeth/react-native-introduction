eff4f863dc842be30a28fd8b3d2f65179fc36fb2
SmpManeth <smpmaneth@gmail.com>
2025-05-14 16:55:00 +0530

    update App.js

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Halooo!</Text>
-      <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
