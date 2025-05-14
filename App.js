74dc5e4849795b8698963fa9153f215b22e26a6f
SmpManeth <smpmaneth@gmail.com>
2025-05-14 17:18:01 +0530

    update App.js

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
-
+import { Text, TouchableOpacity } from "react-native";
+import { Profile } from "./components/Profile/Profile";
 
 export default function App() {
-
+  return (
+    <>
+      <Text>Welcome to React Native!</Text>
+      <Profile />
+    </>
+  );
 }
-
---
+++ components/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import { TouchableOpacity , Text } from "react-native";
+
+export function Profile() {
+  return (
+    <>
+      <Text>Welcome to React Native!</Text>
+      <TouchableOpacity>
+        <Text>Click Me</Text>
+      </TouchableOpacity>
+    </>
+  );
+}
