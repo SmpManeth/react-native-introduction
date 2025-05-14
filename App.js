d53aaa9f55cb89b36b1ca113cd2fc9818a87c9e6
SmpManeth <smpmaneth@gmail.com>
2025-05-14 18:04:24 +0530

    update App.js and App.styles.js

--- App.js
+++ App.js
@@ -5,12 +5,10 @@ import { View } from "react-native";
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ backgroundColor: "red", flex: 1 }}>
-        <View style={{ backgroundColor: "blue", flex: 1 }}>
-          <View style={{ backgroundColor: "yellow", flex: 2 }}></View>
-          <View style={{ backgroundColor: "pink", flex: 3 }}></View>
-        </View>
-        <View style={{ backgroundColor: "green", flex: 3 }}></View>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={ styles.box1  }></View>
+        <View style={ styles.box2  }></View>
+        <View style={ styles.box3  }></View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- App.styles.js
+++ App.styles.js
@@ -2,6 +2,21 @@ import { StyleSheet } from "react-native";
 
 export const  styles = StyleSheet.create({
 
+    box1:{
+        height: 100,
+        width: 100,
+        backgroundColor: "red",
+    },
+    box2:{
+        height: 100,
+        width: 100,
+        backgroundColor: "blue",
+    },
+    box3:{
+        height: 100,
+        width: 100,
+        backgroundColor: "green",
+    },
 
   })
   
