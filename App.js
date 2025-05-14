97a64f7e6fb2929b312cfa20529c63772dcb8e23
SmpManeth <smpmaneth@gmail.com>
2025-05-14 18:00:36 +0530

    update App.js and App.styles.js

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import { Text, View } from "react-native";
-import { Profile } from "./components/Profile/Profile";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { styles } from "./App.styles";
+import { View } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-        <Text style={styles.text}>Welcome to React Native!</Text>
-        <View style={styles.square}>
-          <Text style={styles.innerText}>Super !</Text>
+      <SafeAreaView style={{ backgroundColor: "red", flex: 1 }}>
+        <View style={{ backgroundColor: "blue", flex: 1 }}>
+          <View style={{ backgroundColor: "yellow", flex: 2 }}></View>
+          <View style={{ backgroundColor: "pink", flex: 3 }}></View>
         </View>
+        <View style={{ backgroundColor: "green", flex: 3 }}></View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- App.styles.js
+++ App.styles.js
@@ -2,24 +2,6 @@ import { StyleSheet } from "react-native";
 
 export const  styles = StyleSheet.create({
 
-    text: {
-      fontSize: 20,
-      color: "#000",
-
-    },
-
-    square:{
-        width: 100,
-        height: 100,
-        backgroundColor: "blue",
-        marginTop: 20,
-    },
-
-    innerText:{
-        fontSize: 20,
-        color: "white",
-        textAlign: "center",
-    }
 
   })
   
